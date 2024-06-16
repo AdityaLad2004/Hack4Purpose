fdb7fd3c6d4a86fca12aaca01b3337d80eff615e
adityalad2004 <aditya.lad.2004@gmail.com>
2024-06-16 13:47:13 +0530

    done navbar

---
+++ src/pages/Patients.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import NavBar from '../components/NavBar'
+
+function Patients() {
+    return (
+        <>
+            <NavBar />
+            <div>Patients</div>
+
+        </>
+
+    )
+}
+
+export default Patients
